58bf39a9e0b7f2d5f349d9818b41e9406e993337
davparrado <davparrado7@gmail.com>
2026-02-03 20:46:29 -0500

    marcador_2

--- programa.js
+++ programa.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([-74,146592, 4,680486 ], 13);
+var map = L.map('map').setView([-74.146592, 4.680486 ], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-74,146592, 4,680486]).addTo(map);
+var marker = L.marker([-74.146592, 4.680486]).addTo(map);
