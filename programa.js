f5e0c9359998533e6d9a49820efe57249f33f57a
davparrado <davparrado7@gmail.com>
2026-02-03 21:22:26 -0500

    html

--- programa.js
+++ programa.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([-74.146592, 4.680486 ], 13);
+var map = L.map('map').setView([4.625058, -74.069493 ], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-74.146592, 4.680486]).addTo(map);
+var marker = L.marker([4.625058, -74.069493]).addTo(map);
